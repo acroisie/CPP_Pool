3b6625a25095da2e039014c33797fc87dd206584
acroisie <acroisie@student.42lyon.fr>
2022-07-26 17:40:04 +0200

    prepare_module02

---
+++ CPP_Module_02/ex00/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 16:51:45 by acroisie          #+#    #+#             */
+/*   Updated: 2022/07/26 16:54:26 by acroisie         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../includes/Fixed.hpp"
+
+int	main(void)
+{
+  Fixed	a;
+  Fixed	b(a);
+  Fixed	c;
+
+  c = b;
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  return 0;
+}
