92b17d91db873a54af0b7b9f45e76ce05ef8b005
Arnaud <acroisie@student.42lyon.fr>
2022-09-27 17:48:03 +0200

    update_07_02

--- CPP_Module_07/ex02/includes/Array.hpp
+++ CPP_Module_07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 10:59:40 by acroisie          #+#    #+#             */
-/*   Updated: 2022/09/27 16:27:41 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/09/27 17:45:06 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,11 @@ public:
 	{
 		delete[] _array;
 		_array = new T[obj._size];
-		_array = obj._array;
 		_size = obj._size;
+		for (size_t i = 0; i < obj._size; i++)
+		{
+			_array[i] = obj._array[i];
+		}
 
 		return (*this);
 	}
@@ -36,8 +39,17 @@ public:
 			throw std::out_of_range("Array index out of range, exiting.");
 		return _array[index];
 	}
+
+	const T& operator[](size_t index) const
+	{
+		if (index >= _size || index < 0)
+			throw std::out_of_range("Array index out of range, exiting.");
+		return _array[index];
+	}
 	
-	Array(void):_array(NULL), _size(0){}
+	Array(void):_size(0){
+		_array = NULL;
+	}
 	
 	Array(unsigned int n)
 	{
@@ -45,10 +57,12 @@ public:
 		_size = (size_t)n;
 	}
 	
-	Array(const Array& cpy)
+	Array(const Array& cpy): _size(cpy._size)
 	{
 		_array = new T[cpy._size];
-		*this = cpy;
+		for (size_t i = 0; i < cpy._size; ++i) {
+			_array[i] = cpy[i];
+		}
 	}
 	
 	~Array()
@@ -56,7 +70,7 @@ public:
 		delete[] _array;
 	}
 
-	size_t	size(void)
+	size_t	size(void) const
 	{
 		return (_size);
 	}
--- CPP_Module_07/ex02/srcs/main.cpp
+++ CPP_Module_07/ex02/srcs/main.cpp
@@ -6,51 +6,63 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 10:59:59 by acroisie          #+#    #+#             */
-/*   Updated: 2022/09/27 16:26:07 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/09/27 17:46:52 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"../includes/Array.hpp"
 
 int main(void)
-{
-    Array<int>          intArray;
-    Array<std::string>  stringArray(5);
-    Array<float>        floatArray(2);
+{    
+	const Array<int>			intArray;
+	Array<std::string>			stringArray(5);
+	Array<float>				floatArray(2);
 
-    stringArray[0] = "Climb";
-    stringArray[1] = "to the summit";
-    stringArray[2] = "then, ";
-    stringArray[3] = "top down";
-    stringArray[4] = "paraglide.";
+	Array<std::string>			peter;
+	const Array<std::string>	peter2;
 
-    floatArray[0] = 0.42;
-    floatArray[1] = 0.0;
+	
+	stringArray[0] = "Climb";
+	stringArray[1] = "to the summit";
+	stringArray[2] = "then, ";
+	stringArray[3] = "top down";
+	stringArray[4] = "paraglide.";
+	
+	const Array<std::string> michel(stringArray);
 
-    std::cout << "intArray size:    " << intArray.size() << std::endl;
-    std::cout << "stringArray size: " << stringArray.size() << std::endl;
-    
-    try
-    {
-        std::cout << stringArray[0] << std::endl;
-        std::cout << stringArray[2] << std::endl;
-        std::cout << stringArray[4] << std::endl;
-        std::cout << stringArray[100] << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+	floatArray[0] = 0.42;
+	floatArray[1] = 0.0;
 
-    try
-    {
-        std::cout << floatArray[1] << std::endl;
-        std::cout << floatArray[-2] << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    
-    return (0);
+	std::cout << "intArray size:    " << intArray.size() << std::endl;
+	std::cout << "stringArray size: " << stringArray.size() << std::endl;
+
+	stringArray = michel;
+
+	peter = peter2;
+	
+	try
+	{
+		std::cout << stringArray[0] << std::endl;
+		std::cout << michel[1] << std::endl;
+		std::cout << stringArray[2] << std::endl;
+		std::cout << michel[3] << std::endl;
+		std::cout << stringArray[4] << std::endl;
+		std::cout << stringArray[100] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << floatArray[1] << std::endl;
+		std::cout << floatArray[-2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	return (0);
 }
