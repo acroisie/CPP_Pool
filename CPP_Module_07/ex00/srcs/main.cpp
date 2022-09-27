6f9fef49ac2c1bf59ad95af032a85b42b2ed7177
Arnaud <acroisie@student.42lyon.fr>
2022-09-27 16:51:00 +0200

    update_mains_07

--- CPP_Module_07/ex00/srcs/main.cpp
+++ CPP_Module_07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:22:46 by acroisie          #+#    #+#             */
-/*   Updated: 2022/09/26 22:28:35 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/09/27 16:37:30 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,26 @@ int	main(void)
 
 	return 0;
 }
+
+// class Awesome
+// {
+// public:
+// Awesome(void) : _n(0) {}
+// Awesome( int n ) : _n( n ) {}
+// Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+// bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); } bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); } bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); } bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); } bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); } bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); } int get_n() const { return _n; }
+// private:
+// int _n;
+// };
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+// int main(void)
+// {
+// Awesome a(2), b(4);
+
+
+// swap(a, b);
+// std::cout << a << " " << b << std::endl; std::cout << max(a, b) << std::endl;
+
+// std::cout << min(a, b) << std::endl; return (0);
+// }
--- CPP_Module_07/ex01/includes/iter.hpp
+++ CPP_Module_07/ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 08:13:13 by acroisie          #+#    #+#             */
-/*   Updated: 2022/09/27 10:46:14 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/09/27 16:46:55 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ template <typename T> void funToExec(T arg)
 	std::cout << arg << std::endl;
 }
 
-template <typename T> void iter(T* array, size_t size, void (*funToExec)(T))
+template <typename T> void iter(T* array, size_t size, void (*funToExec)(const T&))
 {
 	for (size_t i = 0; i < size; i++)
 	{
